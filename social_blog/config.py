dec990e607f2a79130fff5c68e38672168203fca
Haroon Waqar <haroonwaqar1234@gmail.com>
2025-07-26 17:20:33 +0500

    changed the config for postgres db

--- social_blog/config.py
+++ social_blog/config.py
@@ -1,8 +1,11 @@
 import os
+password = os.environ.get('POSTGRES_PASS')
 
 class Config:
    SECRET_KEY = os.environ.get('SECRET_KEY')
-   SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+   SQLALCHEMY_DATABASE_URI = f'postgresql://postgres:{password}@localhost:5432/Social_Blog_db'
+   SQLALCHEMY_TRACK_MODIFICATIONS = False
+   
    MAIL_SERVER = 'smtp.gmail.com'
    MAIL_PORT = 587
    MAIL_USE_TLS = True
