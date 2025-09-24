aca85bac3931232601d6dbbad2e7bb53b59fcf3d
Haroon Waqar <haroonwaqar1234@gmail.com>
2025-09-24 23:48:16 +0500

    adding environ variable

--- social_blog/config.py
+++ social_blog/config.py
@@ -3,7 +3,7 @@ password = os.environ.get('POSTGRES_PASS')
 
 class Config:
    SECRET_KEY = os.environ.get('SECRET_KEY')
-   SQLALCHEMY_DATABASE_URI = f'postgresql://socialblogdb_user:{password}@dpg-d3a34bumcj7s73e3j11g-a/socialblogdb'
+   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
    SQLALCHEMY_TRACK_MODIFICATIONS = False
    #postgresql://postgres:{password}@localhost:5432/Social_Blog_db
    MAIL_SERVER = 'smtp.gmail.com'
